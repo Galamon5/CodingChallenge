fcc3db7dda6d6c78b03365e3570b3370dbf797a3
Galamon Mac <galamon5@hotmail.com>
2019-01-09 01:06:59 -0600

    Validators on /orders added

--- server/orders.js
+++ server/orders.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
-const { check } = require('express-validator/check')
+const { check, validationResult } = require('express-validator/check');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 var orders = [];
 
 const generateOrders = (req, res, next) =>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   if(orders.length===0 && req.body.genOrders) {
     console.log('Genereting random orders...');
     let randomOrder = (i) => {
@@ -36,6 +40,10 @@ const generateOrders = (req, res, next) =>{
 }
 
 const getOrders = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   if(req.query.page) {
    next();
    return;
@@ -51,6 +59,10 @@ const getOrders = (req, res, next) => {
 }
 
 const getPageOrders = (req, res) =>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   let num_page = parseInt(req.query.page);
   let response = {
     size: orders.slice(num_page*5,(num_page*5)+5).length,
@@ -63,21 +75,38 @@ const getPageOrders = (req, res) =>{
 }
 
 const updateTips = (req, res) =>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
   let id = parseInt(req.body.id);
   let tip = parseFloat(req.body.tip).toFixed(2);
   tip = parseFloat(tip);
-  if(orders.some((order)=> order.Id===id)){
-    orders[orders.findIndex((order) => order.Id===id)].Tip = tip;
-    orders[orders.findIndex((order) => order.Id===id)].Total += tip;
-    orders[orders.findIndex((order) => order.Id===id)].Total = parseFloat(orders[orders.findIndex((order) => order.Id===id)].Total.toFixed(2));
-    res.send(orders[orders.findIndex((order) => order.Id===id)]);
-  }else{
-    res.send('hola');
-  }
+  orders[orders.findIndex((order) => order.Id===id)].Tip = tip;
+  orders[orders.findIndex((order) => order.Id===id)].Total += tip;
+  orders[orders.findIndex((order) => order.Id===id)].Total = parseFloat(orders[orders.findIndex((order) => order.Id===id)].Total.toFixed(2));
+  res.send(orders[orders.findIndex((order) => order.Id===id)]);
 }
 
 router.route('/')
-  .get([check('page').isNumeric(),check('genOrders').isBoolean(),generateOrders,getOrders,getPageOrders])
-  .patch([updateTips]);
+  .get([
+    check('page').optional().isInt(),
+    check('genOrders').optional().isBoolean(),
+    generateOrders,
+    getOrders,
+    getPageOrders])
+  .patch([
+    check('id').isInt({ min: 0 }).withMessage('must be at least 0').custom((value,{ req }) => {
+      if(!orders.some((order)=> order.Id==value))
+        throw new Error('Id not found');
+      else return true;
+    }),
+    check('tip').isFloat({ min: 0 }).withMessage('must be at least 0').custom((value,{ req }) => {
+      let id = parseInt(req.body.id);
+      if(orders[orders.findIndex((order) => order.Id===id)].Tip>0)
+        throw new Error('This order already has a tip');
+      else return true;
+    }),
+    updateTips]);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const orders = require('./orders');
 
 app.use('/orders', orders);
-
 const server = app.listen(8080, () => {
     console.log('Server is running..');
 });
